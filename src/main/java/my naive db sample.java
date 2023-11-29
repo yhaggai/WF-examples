453b7ffba6a4a66ff5d0a0702cfe36718f4806c2
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2023-11-29 13:53:20 -0500

    Update my naive db sample.java

--- src/main/java/my naive db sample.java
+++ src/main/java/my naive db sample.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-
 public class SQLInjectionExample extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
